039587da4fd294100390ed4e6eb08df990ac9a0a
Bok Young Kim <bk294@cornell.edu>
2013-12-14 16:17:17 -0500

    cool

--- jtov.py
+++ jtov.py
@@ -3,9 +3,12 @@ import Image
 
 def jtov(filename):
 	f = Image.open(filename)
+	f.show()
 	newF = f.crop((0, 0, 240, 240))
 	finalF = newF.convert('L')
+	finalF = finalF.resize((28, 28))
 	finalF.save("gray" + filename)
+	print list(finalF.getdata())
 
 
 jtov("photo.JPG")
