c9f9d90a4fe5035000d9e602a71ea95e49480b2f
cheelan <cjh276@cornell.edu>
2013-12-14 16:08:50 -0500

    jtov works

--- handwriting.py
+++ handwriting.py
@@ -2,6 +2,8 @@ import read_data
 import math
 import cProfile
 import pylab as pl
+import otsu
+import numpy
 #See http://scikit-learn.org/stable/modules/generated/sklearn.cross_validation.cross_val_score.html
 from sklearn import cross_validation
 from sklearn import svm
@@ -10,11 +12,21 @@ from sklearn.ensemble import ExtraTreesClassifier
 
 
 #Read dataset
-train_images, train_labels = read_data.read(range(10), 'training')
-test_images, test_labels = read_data.read(range(10), 'testing')
+train_images_gray, train_labels = read_data.read(range(10), 'training')
+test_images_gray, test_labels = read_data.read(range(10), 'testing')
 print "Done reading data"
 
-def svms(images, labels, cs):
+def convert_bw(images):
+  bw = []
+  for i in images:
+    bw.append(numpy.array(otsu.otsu(i)))
+
+train_images_bw = convert_bw(train_images_gray)
+test_images_bw = convert_bw(test_images_gray)
+
+
+
+def svms(images, labels, test_images, test_labels, cs):
   for training_penalty in cs:
     clf = svm.SVC(C=training_penalty)
     #scores = cross_validation.cross_val_score(clf, images, labels, cv=5)
@@ -23,7 +35,7 @@ def svms(images, labels, cs):
     #print "Testing SVM kernel = " + k + " c = " + str(training_penalty) + " " + str(scores)
 
 #Doesn't necessarily use all of max_depth. TDIDT stops once all training examples are classified correctly
-def decision_trees(images, labels, depths, showimage=False):
+def decision_trees(images, labels, test_images, test_labels, depths, showimage=False):
     for d in depths:
       print d
       clf = DecisionTreeClassifier(max_depth = d)
@@ -32,7 +44,7 @@ def decision_trees(images, labels, depths, showimage=False):
 
 #The main parameters to adjust when using these methods is n_estimators and max_features. The former is the number of trees in the forest. The larger the better, but also the longer it will take to compute. In addition, note that results will stop getting significantly better beyond a critical number of trees. The latter is the size of the random subsets of features to consider when splitting a node. The lower the greater the reduction of variance, but also the greater the increase in bias. 
 #See Feature importance evaluation: http://scikit-learn.org/stable/modules/ensemble.html#forests-of-randomized-trees
-def random_forests(images, labels, estimators, depths, showimage=False):
+def random_forests(images, labels, test_images, test_labels, estimators, depths, showimage=False):
   for e in estimators:
     for d in depths:
       clf = ExtraTreesClassifier(n_estimators=e, max_depth=d,compute_importances=showimage) #num_estimators=e, max_features=f  
@@ -48,7 +60,14 @@ def random_forests(images, labels, estimators, depths, showimage=False):
 
 
 #cProfile.run("svms(train_images[:10000], train_labels[:10000], [1], ['poly'])")
-svms(train_images, train_labels, [0.01, 0.1, 1, 10, 100])
-random_forests(train_images, train_labels, [1, 10, 50, 100], [2, 4, 8, 16, 32], False)
-decision_trees(train_images, train_labels, [2, 4, 8, 16, 32, 64, 128])
+# print "BLACK WHITE"
+# svms(train_images_bw, train_labels, test_images_bw, test_labels, [0.01, 0.1, 1, 10, 100])
+# random_forests(train_images_bw, train_labels, test_images_bw, test_labels, [1, 10, 50, 100], [2, 4, 8, 16, 32], False)
+# decision_trees(train_images_bw, train_labels, test_images_bw, test_labels, [2, 4, 8, 16, 32, 64, 128])
+
+print "GRAYSCALE"
+svms(train_images_gray, train_labels, test_images_gray, test_labels, [0.01, 0.1, 1, 10, 100])
+random_forests(train_images_gray, train_labels, test_images_gray, test_labels, [1, 10, 50, 100], [2, 4, 8, 16, 32], False)
+decision_trees(train_images_gray, train_labels, test_images_gray, test_labels [2, 4, 8, 16, 32, 64, 128])
+
 
--- jtov.py
+++ jtov.py
@@ -3,54 +3,9 @@ import Image
 
 def jtov(filename):
 	f = Image.open(filename)
-	newF = f.crop((240, 240))
-	finalF = newF.convert('LA')
+	newF = f.crop((0, 0, 240, 240))
+	finalF = newF.convert('L')
 	finalF.save("gray" + filename)
 
 
-
-def otsu(image):
-	total = len(image)
-	histogram = [0] * 256
-
-	for i in image:
-		histogram[i] += 1
-
-	sumA = 0
-
-	for i in range(len(histogram)):
-		sumA += i * histogram[i]
-
-	sumB = 0
-	wB = 0
-	wF = 0
-	maximum = 0
-	threshold = 0
-
-	for i in range(len(histogram)):
-		wB += histogram[i]
-
-		if wB == 0:
-			continue
-
-		wF = total - wB
-
-		if wF == 0:
-			break
-
-		sumB += i * histogram[i]
-		mB = sumB / wB
-		mF = (sumA - sumB) / wF
-		between = wB * wF * (mB - mF) * (mB - mF)
-
-		if between > maximum:
-			maximum = between
-			threshold = i
-
-	newImage = [0] * total
-
-	for i in range(total):
-		if image[i] > threshold:
-			newImage[i] = 1
-
-	return newImage
+jtov("photo.JPG")
