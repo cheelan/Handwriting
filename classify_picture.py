21190d9e2a6a9a3ea424c8a321e41740884ba7da
cheelan <cjh276@cornell.edu>
2013-12-14 19:41:09 -0500

    bw images for image_reader

--- classify_picture.py
+++ classify_picture.py
@@ -1,17 +1,25 @@
 import jtov
 import numpy
 import read_data
+import otsu
 from sklearn import cross_validation
 from sklearn import svm
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import ExtraTreesClassifier
 
+def convert_bw(images):
+  bw = []
+  for i in images:
+    bw.append(numpy.array(otsu.otsu(i)))
+  return numpy.array(bw)
+
 def classify(image):
 	train_images_gray, train_labels = read_data.read(range(10), 'training')
+	train_images_bw = convert_bw(train_images_gray)
 	test = numpy.array(jtov.jtov(image))
 	print test
 	clf = svm.SVC(kernel="poly", degree=2)
-	clf.fit(train_images_gray, train_labels)
+	clf.fit(train_images_bw, train_labels)
 	print clf.predict(test)
 
 classify("photo.JPG")
